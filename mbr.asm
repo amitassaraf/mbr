b3581986b90e17f68e3bebf05eb254ff7c7f6bdc
Amit Assaraf <amit@helpi.me>
2016-10-10 01:07:24 +0300

    Fixed to MBR, now working

--- mbr.asm
+++ mbr.asm
@@ -1,6 +1,11 @@
 [bits 16]
 [org 0x7C00]
 
+disable_a20:         ; Disable A20 in the begining
+	in al, 0x92
+    and al, 0xfd
+    out 0x92, al
+
 jmp check_a20_enabled
 
 enable_a20:
@@ -9,34 +14,36 @@ enable_a20:
     out 0x92, al
 
 check_a20_enabled:
-	mov dx, 0FFFFh
+	mov dx, 0xffff
 	mov es, dx
 	xor dx, dx
-	mov dx, [es:010h]
-	add dx, 01h
-	not [dx]
-	cmp dx, [es:010h]
-	je a20_disabled
-	jne a20_enabled
+	mov byte dl, [es:0x10] ; Save overflow value
+	mov al, dl   
+	not al                 
+	mov [0], al            ; Change original value to notted overflow value
+	mov byte al, [es:0x10]
+	cmp al, dl             ; Compare new notted value to original
+	jne a20_disabled       ; If they are different A20 is disabled
+	je a20_enabled         ; If they are equal then A20 is enabled 
 
 a20_enabled:
 	xor bh, bh
-	mov cx, 01h
-	mov al, 079h
-	mov ah, 00Ah
-	int 10h
+	mov cx, 0x1
+	mov al, 0x79           ; Print 'y' to the screen to indicated A20 is on
+	mov ah, 0x0e
+	int 0x10
 	jmp exit
 
 a20_disabled:
 	xor bh, bh
-	mov cx, 01h
-	mov al, 06eh
-	mov ah, 00Ah
-	int 10h
+	mov cx, 0x1
+	mov al, 0x6e           ; Print 'n' to the screen to indicated A20 is off
+	mov ah, 0x0e
+	int 0x10
 	jmp enable_a20
 
 exit:
 	nop
 
 times 510 - ($ - $$) db 0
-dw 0xAA55
+dw 0xAA55                  ; MBR Magic
