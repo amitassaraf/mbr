fd234d89d8b1932a95f50b57c6b06deb4cc2563e
Amit Assaraf <amit@helpi.me>
2016-10-22 10:49:56 +0300

    Changed to use functions

--- mbr.asm
+++ mbr.asm
@@ -1,17 +1,36 @@
 [bits 16]
 [org 0x7C00]
 
-disable_a20:         ; Disable A20 in the begining
-	in al, 0x92
-    and al, 0xfd
-    out 0x92, al
+A20_ENABLED equ 0x1
+A20_DISABLED equ 0x0
 
-jmp check_a20_enabled
+
+mbr:						; Main sequence
+	mov sp, 0x500
+	call disable_a20        ; Disable A20 in the beggining
+check:
+	call check_a20_enabled  ; Check if A20 is enabled
+	cmp eax, A20_ENABLED
+	jne disabled
+	call print_enabled
+	jmp exit				; We enabled A20 successfuly, exit
+disabled:
+	call print_disabled
+	call enable_a20
+	jmp check
+
+
+disable_a20:       
+	in al, 0x92
+	and al, 0xfd
+	out 0x92, al
+	ret
 
 enable_a20:
 	in al, 0x92
-    or al, 2
-    out 0x92, al
+	or al, 2
+	out 0x92, al
+	ret
 
 check_a20_enabled:
 	mov dx, 0xffff
@@ -25,24 +44,40 @@ check_a20_enabled:
 	cmp al, dl             ; Compare new notted value to original
 	jne a20_disabled       ; If they are different A20 is disabled
 	je a20_enabled         ; If they are equal then A20 is enabled 
-
+a20_disabled:
+	mov eax, A20_DISABLED
+	ret
 a20_enabled:
+	mov eax, A20_ENABLED
+	ret
+
+print_enabled:
 	xor bh, bh
 	mov cx, 0x1
 	mov al, 0x79           ; Print 'y' to the screen to indicated A20 is on
 	mov ah, 0x0e
 	int 0x10
-	jmp exit
+	ret
 
-a20_disabled:
+print_disabled:
 	xor bh, bh
 	mov cx, 0x1
 	mov al, 0x6e           ; Print 'n' to the screen to indicated A20 is off
 	mov ah, 0x0e
 	int 0x10
-	jmp enable_a20
+	ret
+
+print_done:
+	xor bh, bh
+	mov cx, 0x1
+	mov al, 0x64           ; Print 'd' to the screen to indicated the MBR finished
+	mov ah, 0x0e
+	int 0x10
+	ret
+
 
 exit:
+	call print_done
 	nop
 
 times 510 - ($ - $$) db 0
